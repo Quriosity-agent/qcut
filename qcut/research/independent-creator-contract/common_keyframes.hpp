5cc2140288433299401c5d6bc23532143e283b9e
Donghao Zhang <zdhpeter@gmail.com>
2026-09-07 17:42:12 +1000

    feat(research): update common keyframes contract

--- qcut/research/independent-creator-contract/common_keyframes.hpp
+++ qcut/research/independent-creator-contract/common_keyframes.hpp
@@ -3,23 +3,37 @@
 #include "../independent-editor-contract/value_state.hpp"
 
 #include <memory>
+#include <cstdint>
 #include <string>
 #include <string_view>
 #include <vector>
 
 namespace creator_contract {
 
+struct KeyframeControl {
+  double x = 0;
+  double y = 0;
+  editor_contract::MutationState mutation;
+};
+
 struct CommonKeyframe {
   std::string id;
   std::vector<double> values;
   bool has_graph = false;
   editor_contract::MutationState mutation;
+  std::int64_t time_offset = 0;
+  std::int32_t curve_type = 0;
+  std::shared_ptr<KeyframeControl> left_control = {};
+  std::shared_ptr<KeyframeControl> right_control = {};
 };
 
 struct CommonKeyframeGroup {
   std::string property;
   std::vector<std::shared_ptr<CommonKeyframe>> keyframes;
   editor_contract::MutationState mutation;
+  std::string material_id = {};
+  bool track_inserted_children = false;
+  editor_contract::MutationState list_mutation = {};
 };
 
 struct CommonKeyframeArray {
