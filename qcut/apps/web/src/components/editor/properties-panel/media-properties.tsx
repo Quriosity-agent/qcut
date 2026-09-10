9cd0a7ae3d62ebd1a1edc8dbd25ed8a842efa352
Donghao Zhang <zdhpeter@gmail.com>
2026-09-10 20:29:31 +1000

    feat(properties): pass the media item to the stabilization section

--- qcut/apps/web/src/components/editor/properties-panel/media-properties.tsx
+++ qcut/apps/web/src/components/editor/properties-panel/media-properties.tsx
@@ -1186,6 +1186,7 @@ export function MediaProperties({
 						<StabilizationSection
 							enhancements={visual.enhancements}
 							onChange={updateEnhancements}
+							mediaItem={mediaItem}
 						/>
 					) : null}
 
